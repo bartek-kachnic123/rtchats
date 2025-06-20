3eab26d2ff63149793fb718bd727cb1447e40a5f
bartek-kachnic123 <bartek.kachnic123@gmail.com>
2025-06-20 19:46:24 +0200

    Initialize project

---
+++ backend/src/main/java/com/kachnic/rtchats/RtChatsApplication.java
@@ -0,0 +1,13 @@
+package com.kachnic.rtchats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RtChatsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RtChatsApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/kachnic/rtchats/RtChatsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kachnic.rtchats;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RtChatsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
