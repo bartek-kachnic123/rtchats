09b07f12cdcab8732ccecd8c9a36a9289e576ea9
bartek-kachnic123 <bartek.kachnic123@gmail.com>
2025-07-01 17:35:15 +0200

    feat: setup Chakra UI with provider and color-mode

---
+++ frontend/src/ui/chakra-ui/components/ColorMode.jsx
@@ -0,0 +1,73 @@
+'use client';
+
+import * as React from 'react';
+import { ClientOnly, IconButton, Skeleton, Span } from '@chakra-ui/react';
+import { LuMoon, LuSun } from 'react-icons/lu';
+import { ThemeProvider } from 'next-themes';
+
+import { useColorMode } from '../hooks/useColorMode.js';
+
+export function ColorModeProvider(props) {
+  return (
+    <ThemeProvider attribute="class" disableTransitionOnChange {...props} />
+  );
+}
+
+export function ColorModeIcon() {
+  const { colorMode } = useColorMode();
+  return colorMode === 'dark' ? <LuMoon /> : <LuSun />;
+}
+
+export const ColorModeButton = React.forwardRef(
+  function ColorModeButton(props, ref) {
+    const { toggleColorMode } = useColorMode();
+    return (
+      <ClientOnly fallback={<Skeleton boxSize="8" />}>
+        <IconButton
+          onClick={toggleColorMode}
+          variant="ghost"
+          aria-label="Toggle color mode"
+          size="sm"
+          ref={ref}
+          {...props}
+          css={{
+            _icon: {
+              width: '5',
+              height: '5',
+            },
+          }}
+        >
+          <ColorModeIcon />
+        </IconButton>
+      </ClientOnly>
+    );
+  },
+);
+
+export const LightMode = React.forwardRef(function LightMode(props, ref) {
+  return (
+    <Span
+      color="fg"
+      display="contents"
+      className="chakra-theme light"
+      colorPalette="gray"
+      colorScheme="light"
+      ref={ref}
+      {...props}
+    />
+  );
+});
+
+export const DarkMode = React.forwardRef(function DarkMode(props, ref) {
+  return (
+    <Span
+      color="fg"
+      display="contents"
+      className="chakra-theme dark"
+      colorPalette="gray"
+      colorScheme="dark"
+      ref={ref}
+      {...props}
+    />
+  );
+});
---
+++ frontend/src/ui/chakra-ui/hooks/useColorMode.js
@@ -0,0 +1,19 @@
+import { useTheme } from 'next-themes';
+
+export function useColorMode() {
+  const { resolvedTheme, setTheme, forcedTheme } = useTheme();
+  const colorMode = forcedTheme || resolvedTheme;
+  const toggleColorMode = () => {
+    setTheme(resolvedTheme === 'dark' ? 'light' : 'dark');
+  };
+  return {
+    colorMode: colorMode,
+    setColorMode: setTheme,
+    toggleColorMode,
+  };
+}
+
+export function useColorModeValue(light, dark) {
+  const { colorMode } = useColorMode();
+  return colorMode === 'dark' ? dark : light;
+}
