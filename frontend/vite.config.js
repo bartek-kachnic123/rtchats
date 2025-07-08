9810aa0c6475b6876a7f2944b4ef9c877026d195
bartek-kachnic123 <bartek.kachnic123@gmail.com>
2025-07-08 23:32:42 +0200

    chore: configure React Testing Library with Vitest (jsdom) and add Vitest coverage

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,9 +2,11 @@ import { ValidateEnv } from '@julr/vite-plugin-validate-env';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
-import { validateEnvConfig } from './validate-env-config.js';
+import { validateEnvConfig } from './validate-env.config.js';
+import vitestConfig from './vitest.config.js';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), ValidateEnv(validateEnvConfig)],
+  vitestConfig,
 });
