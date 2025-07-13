b873f039230bac00ee33cb0bb80e78415ff3bfab
bartek-kachnic123 <bartek.kachnic123@gmail.com>
2025-07-13 17:42:28 +0200

    chore: setup linting, formatting, and pre-commit for top-level files

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,12 @@
+import js from '@eslint/js';
 import { defineConfig } from 'eslint/config';
+import prettier from 'eslint-config-prettier';
 import simpleImportSort from 'eslint-plugin-simple-import-sort';
-import { frontendEslintConfig, frontendIgnoredFiles } from './frontend/eslint-frontend.config.js';
+
+import {
+  frontendEslintConfig,
+  frontendIgnoredFiles,
+} from './frontend/eslint-frontend.config.js';
 
 const sharedConfig = {
   files: [],
@@ -21,10 +27,22 @@ const sharedConfig = {
   },
 };
 
-sharedConfig.files = Array.from(new Set([...sharedConfig.files, ...frontendEslintConfig.files]));
+const rootConfig = {
+  files: ['*.js'],
+  extends: [js.configs.recommended, prettier],
+};
+
+sharedConfig.files = Array.from(
+  new Set([
+    ...sharedConfig.files,
+    ...rootConfig.files,
+    ...frontendEslintConfig.files,
+  ]),
+);
 
 export default defineConfig([
   frontendIgnoredFiles,
   sharedConfig,
+  rootConfig,
   frontendEslintConfig,
 ]);
