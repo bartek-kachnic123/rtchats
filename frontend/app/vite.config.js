f780ad20e5d22ccd1d57f64ed9f936292835a177
bartek-kachnic123 <bartek.kachnic123@gmail.com>
2025-07-30 22:05:04 +0200

    refactor: add @src alias in vite

--- frontend/app/vite.config.js
+++ frontend/app/vite.config.js
@@ -1,12 +1,22 @@
 import { ValidateEnv } from '@julr/vite-plugin-validate-env';
 import react from '@vitejs/plugin-react';
+import path from 'path';
+import { fileURLToPath } from 'url';
 import { defineConfig } from 'vite';
 
 import { validateEnvConfig } from './config/validate-env.config.js';
 import vitestConfig from './config/vitest.config.js';
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), ValidateEnv(validateEnvConfig)],
   ...vitestConfig,
+  resolve: {
+    alias: {
+      '@src': path.resolve(__dirname, 'src'),
+    },
+  },
 });
