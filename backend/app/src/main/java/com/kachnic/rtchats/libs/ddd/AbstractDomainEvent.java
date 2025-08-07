f2a6263b0941c2eb93636c2cf1ea5ab6b2cf4943
bartek-kachnic123 <bartek.kachnic123@gmail.com>
2025-08-07 15:06:16 +0200

    feat: add user created domain event

---
+++ backend/app/src/main/java/com/kachnic/rtchats/libs/ddd/AbstractDomainEvent.java
@@ -0,0 +1,19 @@
+package com.kachnic.rtchats.libs.ddd;
+
+import java.time.Instant;
+import java.util.UUID;
+
+public class AbstractDomainEvent implements DomainEvent {
+    protected final Instant occurredAt = Instant.now();
+    protected final UUID eventId = UUID.randomUUID();
+
+    @Override
+    public Instant getOccurredAt() {
+        return occurredAt;
+    }
+
+    @Override
+    public UUID getEventId() {
+        return eventId;
+    }
+}
