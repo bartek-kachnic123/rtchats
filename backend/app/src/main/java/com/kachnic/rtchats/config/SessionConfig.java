73371c2d31a1d30d8d3447abba7004e0153d118c
Bartłomiej Kachnic <104365494+bartek-kachnic123@users.noreply.github.com>
2025-11-24 15:46:50 +0100

    feat: add Redis-backed session (#25)

---
+++ backend/app/src/main/java/com/kachnic/rtchats/config/SessionConfig.java
@@ -0,0 +1,32 @@
+package com.kachnic.rtchats.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.BeanClassLoaderAware;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializer;
+import org.springframework.lang.NonNull;
+import org.springframework.security.jackson2.SecurityJackson2Modules;
+
+@Configuration
+public class SessionConfig implements BeanClassLoaderAware {
+
+    private ClassLoader loader;
+
+    @Bean
+    public RedisSerializer<Object> springSessionDefaultRedisSerializer() {
+        return new GenericJackson2JsonRedisSerializer(objectMapper());
+    }
+
+    private ObjectMapper objectMapper() {
+        final ObjectMapper mapper = new ObjectMapper();
+        mapper.registerModules(SecurityJackson2Modules.getModules(loader));
+        return mapper;
+    }
+
+    @Override
+    public void setBeanClassLoader(@NonNull final ClassLoader loader) {
+        this.loader = loader;
+    }
+}
